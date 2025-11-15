e9ce38bdff177c756ab1ce025b442aa48df29b22
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-11-14 21:38:53 -0500

    NOA-PRIMER COMMIT-RESTAURANTE

---
+++ restaurante/restaurante/src/main/java/com/saborgourmet/restaurante/SaborGourmetApplication.java
@@ -0,0 +1,11 @@
+package com.saborgourmet.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborGourmetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaborGourmetApplication.class, args);
+    }
+}
